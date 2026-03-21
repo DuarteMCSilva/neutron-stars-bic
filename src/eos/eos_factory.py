833b11a23c7f0955b403a2e2cd132031d1f25b41
DuarteMCSilva <duartemcsilva@gmail.com>
2026-03-21 11:44:27 +0100

    Document EoS generator methods & cleanup

--- src/eos/eos_factory.py
+++ src/eos/eos_factory.py
@@ -2,10 +2,15 @@ import numpy as np
 import pandas as pd
 
 class EquationOfStateFactory:
+    '''Factory for generating complete Equations of State (EoS) for neutron stars. 
+    This factory takes into account the crust EoS and generates the core EoS based on random properties while ensuring physical consistency.'''
+
     min_c = 0.02 
     max_c = 0.98
-    n_tr  = 0.15
+    n_min  = 0.15
     n_sat = 12*0.16
+
+    n_thresholds = np.array([n_min, n_sat])
 
     def __init__(self, n0, e0, p0, c0, state_transitions_nr = 6):
         self.n0 = n0
@@ -15,6 +20,17 @@ class EquationOfStateFactory:
         self.quantity = state_transitions_nr
 
     def generate(self, crust_eos: pd.DataFrame, model_id: int) -> pd.DataFrame:
+        '''
+            Generates a complete Equation of State by combining the crust EoS with a generated core EoS.
+
+            Parameters:
+                - crust_eos:  A DataFrame containing the crust EoS data. It should have columns 'rho', 'e', 'p', and 'VS' representing the density, energy density, pressure, and velocity of sound, respectively.
+                
+                - model_id: An integer representing the ID of the model.
+            Returns:
+                - A DataFrame containing the complete EoS data, including both the crust and core. The DataFrame will have columns 'rho', 'e', 'p', 'VS', and 'id', where 'id' corresponds to the model_id parameter.
+        '''
+
         core_eos = self.generate_core_eos()
         #$AddingCrust
         crust_eos = crust_eos[:-2]  #Adding the crust (except the last line, which is repeated)
@@ -65,16 +81,30 @@ class EquationOfStateFactory:
         return dataframe # Dataframe Columns: {'rho','Velocity of Sound','drho'}
     
     def generate_sound_velocities(self):
+        '''
+            For each state transition, we generate a random sound velocity (in units of c) between physical limits.
+            
+            **Physical Assumptions**:
+                - The sound velocity should be between 0 and 1 (in units of c).
+        '''
+
         A = np.random.random(self.quantity)*(self.max_c-self.min_c) + self.min_c
         A = np.append(self.c0,A)
         return A
     
     def generate_particle_densities(self):
-        thresholds = np.array([self.n_tr,self.n_sat])
+        '''
+            For each state transition, we generate a random particle density between physical limits.
+            
+            **Physical Assumptions**:
+                - n is nondecreasing with each transition (increasing depth);
+                - n should not exceed n_sat;
+                - The first transition occurs at the crust-core boundary (n = n_min).
+        '''
         A = np.random.random(self.quantity)
 
-        n_points = A*(self.n_sat-self.n_tr) + self.n_tr
-        n_points = np.append(thresholds,n_points)
+        n_points = A*(self.n_sat-self.n_min) + self.n_min
+        n_points = np.append(self.n_thresholds, n_points)
         n_points = np.sort(n_points)
 
         return n_points
