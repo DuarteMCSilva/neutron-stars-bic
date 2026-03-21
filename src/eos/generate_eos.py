739784a1e08295413fd132ca2cc7e0edf173ec69
DuarteMCSilva <duartemcsilva@gmail.com>
2026-03-21 19:17:03 +0100

    generate upon explicit call

--- src/eos/generate_eos.py
+++ src/eos/generate_eos.py
@@ -29,24 +29,6 @@ import utils.date_utils as date_utils
 from eos.eos_factory import EquationOfStateFactory
 
 #############################$Input#############################
-n = 1  #How many EoS to be generated  (%PAR)
-should_create_file = True
-
-crust_boundary_rho = 0.15 #Density at the crust-core boundary (%PAR)
-data = pd.read_csv('./data/crust.csv')
-
-#############################$Organization#############################
-df = pd.DataFrame(data)
-df = df.loc[(df['n']<= crust_boundary_rho)] #%PAR 
-
-keys = data.keys()
-df = df.rename( {keys[0]: "rho" }, axis = 1)
-
-
-#Insert a column for the Velocity of Sound
-de = np.append(np.nan,np.diff(df.e))
-dp = np.append(np.nan,np.diff(df.p))
-df['VS'] = np.sqrt(dp/de)
 
 def get_crust_boundary_conditions(df):
   last_row = df.iloc[-1,]
@@ -61,21 +43,38 @@ def get_crust_boundary_conditions(df):
 
   return n0, e0, p0, c0
 
-n0, e0, p0, c0 = get_crust_boundary_conditions(df)
-#############################$Execution#############################
-dataset = pd.DataFrame()
-eos_factory = EquationOfStateFactory(n0, e0, p0, c0, state_transitions_nr = 6)
+def generate_eos_files(n, should_create_file):
+  crust_boundary_rho = 0.15 #Density at the crust-core boundary (%PAR)
+  data = pd.read_csv('./data/crust.csv')
 
-for i in range(n):
-  i_EoS = eos_factory.generate(df, i) # $OneEoS
-  dataset = pd.concat([dataset, i_EoS], ignore_index=True)
-  if i%37==0: 
-    print(i)
+  #############################$Organization#############################
+  df = pd.DataFrame(data)
+  df = df.loc[(df['n']<= crust_boundary_rho)] #%PAR 
 
-#############################$Saving#############################
-file_name = date_utils.get_current_date_string() + str('.csv')
-target_dir = str("./data/output_eos/")
-if should_create_file == True:
-  dataset.to_csv(target_dir + file_name, sep=" ", index= False)
+  keys = data.keys()
+  df = df.rename( {keys[0]: "rho" }, axis = 1)
 
-  print("saved to the file: ", file_name)
+
+  #Insert a column for the Velocity of Sound
+  de = np.append(np.nan,np.diff(df.e))
+  dp = np.append(np.nan,np.diff(df.p))
+  df['VS'] = np.sqrt(dp/de)
+
+  n0, e0, p0, c0 = get_crust_boundary_conditions(df)
+  #############################$Execution#############################
+  dataset = pd.DataFrame()
+  eos_factory = EquationOfStateFactory(n0, e0, p0, c0, state_transitions_nr = 6)
+
+  for i in range(n):
+    i_EoS = eos_factory.generate(df, i) # $OneEoS
+    dataset = pd.concat([dataset, i_EoS], ignore_index=True)
+    if i%37==0: 
+      print(i)
+
+  #############################$Saving#############################
+  file_name = date_utils.get_current_date_string() + str('.csv')
+  target_dir = str("./data/output_eos/")
+  if should_create_file == True:
+    dataset.to_csv(target_dir + file_name, sep=" ", index= False)
+
+    print("saved to the file: ", file_name)
